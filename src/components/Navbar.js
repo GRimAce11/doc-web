5562b78516490da8a5ae386f1efb07db70c4bc2b
Chethan Nayak <chethannayak010@gmail.com>
2023-05-19 12:15:11 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,16 +9,19 @@ const Navbar = () => {
       <header>
         <nav>
           <div className="logo">
-            <Link to = "/"><img src={lg} style={{ borderRadius: '30px' }} alt="Logo" /></Link>
+            <Link to="/"><img src={lg} style={{ borderRadius: '30px' }} alt="Logo" /></Link>
           </div>
           <ul className="nav-links">
-            <div className="d-flex flex-row">
-              <li className="p-2"><Link to ="/research" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Research</Link></li>
-              <li className="p-2"><Link to ="/publications" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Publication</Link></li>
-              <li className="p-2"><Link to ="/news" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Projects</Link></li>
-              <li className="p-2"><Link to ="/contactus" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>About Us</Link></li>
-              <li className="p-2"><Link to ="/contactus" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Contact Us</Link></li>
+            <input type="checkbox" id="check" />
+            <div className="nav-box">
+              <li className="p-2"><Link to="/research" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Research</Link></li>
+              <li className="p-2"><Link to="/publications" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Publication</Link></li>
+              <li className="p-2"><Link to="/news" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Projects</Link></li>
+              <li className="p-2"><Link to="/contactus" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>About Us</Link></li>
+              <li className="p-2"><Link to="/contactus" style={{ fontFamily: '"Bruno Ace SC", cursive' }}>Contact Us</Link></li>
+              <label htmlFor="check" className="close-menu"><i className="fa-solid fa-xmark" style={{ color: '#ffffff' }} /></label>
             </div>
+            <label htmlFor="check" className="open-menu"><i className="fa-solid fa-bars" style={{ color: '#ffffff' }} /></label>
           </ul>
         </nav>
       </header>
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,52 +1,25 @@
-// import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import "../styles/upload.css"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import "../styles/upload.css"
 
-// class Upload extends Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         file: null,
-//       };
-//       this.handleFileChange = this.handleFileChange.bind(this);
-//     }
-//     handleFileChange(event) {
-//         const file = event.target.files[0];
-//         const reader = new FileReader();
-//         reader.onload = () => {
-//           const fileData = reader.result;
-//           const fileExtension = file.name.split('.').pop().toLowerCase();
-//           const validExtensions = ['dcm'];
-//           if (validExtensions.indexOf(fileExtension) !== -1) {
-//             this.setState({ file: fileData });
-//             this.props.onValidFile();
-//           } else {
-//             alert('Invalid file type!');
-//             this.props.onInvalidFile();
-//           }
-//         };
-//         reader.readAsDataURL(file);
-//       }
-      
+const Upload = () => {
+    return (
+        <>
+            <div className="card position-absolute top-50 start-50 translate-middle bg-transparent" style={{ border: 'none' }}>
+                <div className="containerup text-center bg-transparent" style={{ height: '80px' }}>
 
-// render() {
-//     return (
-//         <>
-//             <div className="card position-absolute top-50 start-50 translate-middle bg-transparent" style={{ border: 'none' }}>
-//                 <div className="containerup text-center bg-transparent" style={{ height: '80px' }}>
-                   
 
-//                     <input type="file" 
-//                     id="upload"     
-//                      hidden onChange={this.handleFileChange} />
-//                     <label htmlFor="upload" className="file-upload-label" style={{ backgroundColor: 'transparent' }}>
-//                         <h2 style={{ color: 'aliceblue' }}>Upload File (.dcm)</h2><i className="fas fa-upload fa-2xl" style={{ color: 'white' }} />
-//                     </label>
-//                 </div>
-//             </div>
+                    <input type="file"
+                        id="upload" accept='.dcm'
+                        hidden />
+                    <label htmlFor="upload" className="file-upload-label" style={{ backgroundColor: 'transparent' }}>
+                        <h2 style={{ color: 'aliceblue' }}>Upload File (.dcm)</h2><i className="fas fa-upload fa-2xl" style={{ color: 'white' }} />
+                    </label>
+                </div>
+            </div>
 
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
-// export default Upload;
+export default Upload
